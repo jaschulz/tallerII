45628818cc1a685f3ee21e7c370360b34b019eb7
jaschulz <javschulz@gmail.com>
2016-09-06 01:41:56 -0300

    changes

--- AppServer/src/main.cpp
+++ AppServer/src/main.cpp
@@ -1,49 +1,71 @@
-#include <stdio.h>
-#include <string.h>
-#include "mongoose/mongoose.h"
-#include <json/json.h>
-#include <json/value.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <mongoose/Server.h>
+#include <mongoose/JsonController.h>
 
-static int handler(struct mg_connection *conn) {
-  char var1[500], var2[500];
+using namespace std;
+using namespace Mongoose;
 
-  if (strcmp(conn->uri, "/handle_post_request") == 0) {
-	if(strcmp(conn->request_method, "POST") != 0) {
-                // send error (we only care about HTTP GET)
-                    mg_send_header(conn, "Content-Type", "text/plain");
-    mg_send_data(conn, "no es POST", strlen("no es POST"));
-                // return not null means we handled the request
-                return 1;
+class MyController : public JsonController
+{
+    public: 
+        void hello(Request &request, JsonResponse &response)
+        {
+            int i;
+
+            for (i=0; i<12; i++) {
+                response["users"][i]["Name"] = "Bob";
             }
 
-    // Send reply to the client, showing submitted form values.
-    // POST data is in conn->content, data length is in conn->content_len
-    mg_send_header(conn, "Content-Type", "text/plain");
-    mg_send_data(conn, "POST ok", strlen("POST ok"));
-/*
-    mg_printf_data(conn,
-                   "Submitted data: [%.*s]\n"
-                   "Submitted data length: %d bytes\n"
-                   "input_1: [%s]\n"
-                   "input_2: [%s]\n",
-                   conn->content_len, conn->content,
-                   conn->content_len, var1, var2);
-  */
-} else {
-    // Show HTML form.
-    mg_send_data(conn, "mal la url", strlen("mal la url"));
-  }
-  return 1;
+            response["timestamp"] = (int)time(NULL);
+        }
+ 
+        void setup()
+        {
+            // Example of prefix, putting all the urls into "/api"
+            setPrefix("/api");
+
+            // Hello demo
+            addRouteResponse("GET", "/", MyController, hello, JsonResponse);
+            addRouteResponse("GET", "/hello", MyController, hello, JsonResponse);
+        }
+};
+
+volatile static bool running = true;
+
+void handle_signal(int sig)
+{
+    if (running) {
+        cout << "Exiting..." << endl;
+        running = false;
+    }
 }
 
-int main(void) {
-  struct mg_server *server = mg_create_server(NULL);
-  mg_set_option(server, "listening_port", "8080");
-  mg_add_uri_handler(server, "/", handler);
-  printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
-  for (;;) {
-    mg_poll_server(server, 1000);
-  }
-  mg_destroy_server(&server);
-  return 0;
+int main()
+{
+    srand(time(NULL));
+
+    signal(SIGINT, handle_signal);
+
+    MyController myController;
+    Server server(8080);
+    server.registerController(&myController);
+    server.setOption("enable_directory_listing", "false");
+    server.start();
+
+    cout << "Server started, routes:" << endl;
+    myController.dumpRoutes();
+
+    while (running) {
+#ifdef WIN32
+		Sleep(10000);
+#else
+        sleep(10);
+#endif
+    }
+
+    server.stop();
+    
+    return EXIT_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-#Instrucciones para correr el AppServer
+#Taller II
 
-Dentro del directorio AppServer, tipear "cmake .". A continuacion, ejecutar "make .". Luego, "sudo make install", y finalmente correr el ejecutable creado.
+# **AppServer** #
 
-Para realizar una request de post, abrir otra consola y ejecutar, por ejemplo, "echo 'request' | \
-curl -X POST -H 'Content-type: text/xml' \
--H 'Accept: text/xml' -d @- http://localhost:8080". 
-
-El Server devolverá el formato que espera, en caso de que no sea el esperado.
-
-También hay un arhivo bash de pueba. Luego de abrir el Server en una consola, abrir otra consola, pararse sobre /AppServer, y ejecutar "sh prueba.sh". 
